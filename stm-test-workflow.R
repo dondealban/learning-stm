5ed2388a7411d32628d53986c6f96aa51fd0ae06
dondealban <dondealban@gmail.com>
2017-09-09 19:10:47 +0800

    Initial commit of script; added header comments

---
+++ stm-test-workflow.R
@@ -0,0 +1,7 @@
+# This script implements a test workflow of a structural topic model using the stm R
+# package (Roberts et al. 2016). The script incorporates some minor modifications in 
+# the original script by Nicholas B. Adams. The link to the source script is found at:  
+# https://github.com/nickbadams/D-Lab_TextAnalysisWorkingGroup/tree/master/STM_prep.
+# 
+# Script modified by: Jose Don T. De Alban
+# Date modified:      09 Sept 2017
