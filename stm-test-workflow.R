e747dfd3efcb06c17376199852415c0549013ca3
dondealban <dondealban@gmail.com>
2017-09-10 13:39:17 +0800

    Saved manyTopics outputs as pdf files

--- stm-test-workflow.R
+++ stm-test-workflow.R
@@ -89,12 +89,44 @@ dev.off()
 # exclusivity values (upper right corner of plot).
 selectedModel3 <- poliblogSelect$runout[[3]] # Choose model #3
 
+# Another option is the 'manyTopics' function that performs model selection across
+# separate STMs that each assume different number of topics. It works the same as 
+# 'selectModel', except user specifies a range of numbers of topics that they want 
+# the model fitted for. For example, models with 5, 10, and 15 topics. Then, for 
+# each number of topics, 'selectModel' is run multiple times. The output is then 
+# processed through a function that takes a pareto dominant run of the model in 
+# terms of exclusivity and semantic coherence. If multiple runs are candidates 
+# (i.e., none weakly dominates the others), a single model run is randomly chosen 
+# from the set of undominated runs.
+storage <- manyTopics(out$documents, out$vocab, K=c(7:10), prevalence=~rating+s(day),
+                      data=meta, runs=10)
+storageOutput1 <- storage$out[[1]] # 7 topics
+pdf("stm-plot-storage-output1.pdf", width=10, height=8.5)
+plot(storageOutput1)
+dev.off()
+storageOutput2 <- storage$out[[2]] # 8 topics
+pdf("stm-plot-storage-output2.pdf", width=10, height=8.5)
+plot(storageOutput2)
+dev.off()
+storageOutput3 <- storage$out[[3]] # 9 topics
+pdf("stm-plot-storage-output3.pdf", width=10, height=8.5)
+plot(storageOutput3)
+dev.off()
+storageOutput4 <- storage$out[[4]] # 10 topics
+pdf("stm-plot-storage-output4.pdf", width=10, height=8.5)
+plot(storageOutput4)
+dev.off()
+
+
 # ALTERNATIVE: MODEL SEARCH ACROSS A NUMBER OF TOPICS.
 # Let R figure out the best model for you defined by exclusivity and semantic 
-# coherence for each K (i.e. # of topics) (here among 10 options per K). The 
-# function 'searchK' uses a data-driven approach to selecting the number of topics.
+# coherence for each K (i.e. # of topics). The function 'searchK' uses a data-driven
+# approach to selecting the number of topics.
 kResult <- searchK(out$documents, out$vocab, K=c(7,10), prevalence=~rating+s(day),
                    data=meta)
 pdf("stm-plot-searchk.pdf", width=10, height=8.5)
 plot(kResult)
 dev.off()
+
+
+
